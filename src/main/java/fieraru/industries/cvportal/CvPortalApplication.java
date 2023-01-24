9a0bc6a30376a3f7b0bcf8ddfa73f54e13fef36d
JohnFieraru <fieraruionutradu@gmail.com>
2023-01-24 15:25:33 +0200

    firstCommit

---
+++ src/main/java/fieraru/industries/cvportal/CvPortalApplication.java
@@ -0,0 +1,13 @@
+package fieraru.industries.cvportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvPortalApplication.class, args);
+	}
+
+}
