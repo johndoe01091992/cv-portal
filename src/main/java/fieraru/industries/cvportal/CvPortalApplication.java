00a0266e57729f966d829f419ec1559a9bd4de32
JohnFieraru <fieraruionutradu@gmail.com>
2023-01-24 16:05:30 +0200

    add User Entity and Jpa Infrastructure

--- src/main/java/fieraru/industries/cvportal/CvPortalApplication.java
+++ src/main/java/fieraru/industries/cvportal/CvPortalApplication.java
@@ -1,9 +1,13 @@
 package fieraru.industries.cvportal;
 
+import fieraru.industries.cvportal.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class CvPortalApplication {
 
 	public static void main(String[] args) {
